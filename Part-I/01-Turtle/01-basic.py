19ae526f1411ebb4743e1c976be37b26f7e082bf
VARUN KUMAR <varunjha089@gmail.com>
2022-01-21 11:29:06 +0530

    basics of turtle completed.

---
+++ Part-I/01-Turtle/01-basic.py
@@ -0,0 +1,20 @@
+from turtle import *
+import time
+
+shape('turtle')
+
+
+# Creating a Square
+# for i in range(4):
+#     forward(100)
+#     right(90)
+
+def square():
+    for i in range(4):
+        forward(100)
+        right(90)
+
+
+square()
+
+time.sleep(5)
