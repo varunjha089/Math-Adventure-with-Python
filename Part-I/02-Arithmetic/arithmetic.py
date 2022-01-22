393593d74a4a1464be1299f57dcaaf8a00451aea
VARUN KUMAR <varunjha089@gmail.com>
2022-01-22 10:56:08 +0530

    Summation arithmetic completed.

--- Part-I/02-Arithmetic/arithmetic.py
+++ Part-I/02-Arithmetic/arithmetic.py
@@ -9,3 +9,13 @@ def average(a, b):
 
 
 print(average(5, 9))
+
+
+def summation(num):
+    running_sum = 0
+    for i in range(1, num + 1):
+        running_sum += i
+    return running_sum
+
+
+print(summation(10))
