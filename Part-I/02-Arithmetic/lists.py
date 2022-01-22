5f25805036328427b08e7a4c9d7b9ca4e6f96980
VARUN KUMAR <varunjha089@gmail.com>
2022-01-22 10:55:49 +0530

    lists arithmetic completed.

---
+++ Part-I/02-Arithmetic/lists.py
@@ -0,0 +1,33 @@
+"""
+Operating on Lists
+"""
+
+
+def add(lists, app):
+    return lists.append(app)
+
+
+def sum_of_two_lists(lis1, lis2):
+    return lis1 + lis2
+
+
+def mul_of_two_lists(lis1, lis2):
+    return lis1 * lis2
+
+
+def remove_from_lists(lis, sub):
+    return lis.remove(sub)
+
+
+def access_individual_item(lis, index):
+    return lis[index]
+
+
+def access_range_in_lists(lis, range1, range2):
+    return lis[range1:range2]
+
+
+def find_the_index(lis, item):
+    return lis.index(item)
+
+
